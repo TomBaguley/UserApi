b3d0ae15dcf0a6de1b9a3f2610954e4952792524
TomBaguley <tombaguley3@gmail.com>
2024-04-17 17:43:29 +0100

    Seeded date works on startup

--- UserApi/Models/UserContext.cs
+++ UserApi/Models/UserContext.cs
@@ -4,8 +4,41 @@ namespace UserApi.Models
 {
     public class UserContext : DbContext
     {
-        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+        public UserContext(DbContextOptions<UserContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
         public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Tom Baguley",
+                    Email = "tom.baguley@barclays.com",
+                    IsAdmin = true,
+                    Password = "tompass",
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Joe Bloggs",
+                    Email = "joe.bloggs@barclays.com",
+                    IsAdmin = false,
+                    Password = "joe123"
+                },
+                new User
+                {
+                    Id = 3, 
+                    Name = "Jane Smith", 
+                    Email = "jane.smith",
+                    IsAdmin = false,
+                    Password = "js!?"
+                }
+                );
+        }
+
 
     }
 }
